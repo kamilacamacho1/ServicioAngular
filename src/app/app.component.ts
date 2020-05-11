b79f240ee09c8d71684533422bfaf638a25f8ab3
Camila Camacho <mccp1030@outlook.com>
2020-05-11 11:43:45 -0500

    Servicio
    Consulta de servicio formato JSON

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticulosService } from './articulos.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'proyecto013';
+
+  articulos = null;
+
+  constructor (private articulosService: ArticulosService ){}
+
+  ngOnInit(){
+    this.articulosService.retornar().subscribe(result => this.articulos = result)
+  }
+}
